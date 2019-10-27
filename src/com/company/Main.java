07d7e2dc40fa2bd6d192361bba6562e3c32a7542
Maksim-17 <56191893+Maksim-17@users.noreply.github.com>
2019-10-28 00:04:11 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,12 @@ public class Main {
     public static void main(String[] args) {
         int x = 1;
 
-        // Когда 'value' еще меньше 10, то цикл продолжает работать.
+        
         while (x < 51) {
 
             System.out.println(+ x);
 
-            // Увеличить значение value на 2 единицы
+            
             x = x + 1;}
     }
 }
