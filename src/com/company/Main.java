371eb0bfc9dcdf565ec86b9a5d8265c306d69a8b
Maxim <dino1786@mail.ru>
2019-10-28 00:05:06 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 1;
+
+        // Когда 'value' еще меньше 10, то цикл продолжает работать.
+        while (x < 51) {
+
+            System.out.println(+ x);
+
+            // Увеличить значение value на 2 единицы
+            x = x + 1;}
+    }
+}
